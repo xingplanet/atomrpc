8a06425ff8d580f07103b4442fa19aaac1cdf54c
xingplanet <wjzdyking@gmail.com>
2020-04-29 16:13:05 +0800

    first commit

--- src/main/java/com/xingplanet/atomrpc/AtomRpcApplication.java
+++ src/main/java/com/xingplanet/atomrpc/AtomRpcApplication.java
@@ -3,11 +3,15 @@ package com.xingplanet.atomrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author xingplanet
+ * @date 2020-04-29
+ */
 @SpringBootApplication
-public class AtomrpcApplication {
+public class AtomRpcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AtomrpcApplication.class, args);
+		SpringApplication.run(AtomRpcApplication.class, args);
 	}
 
 }
