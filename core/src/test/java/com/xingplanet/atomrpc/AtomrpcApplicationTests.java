178d08639362116b7739783e8674d257bee7b0a9
xingplanet <wjzdyking@gmail.com>
2020-04-29 17:20:14 +0800

    second commit

--- core/src/test/java/com/xingplanet/atomrpc/AtomrpcApplicationTests.java
+++ core/src/test/java/com/xingplanet/atomrpc/AtomrpcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AtomrpcApplicationTests {
+class AtomRpcApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ test/src/main/java/com/xingplanet/atomrpc/AtomRpcApplication.java
@@ -0,0 +1,17 @@
+package com.xingplanet.atomrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xingplanet
+ * @date 2020-04-29
+ */
+@SpringBootApplication
+public class AtomRpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomRpcApplication.class, args);
+	}
+
+}
